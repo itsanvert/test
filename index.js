d69ab014c0439f98947a7f79b8f9fd1fd8c2cdec
sanvert <itsanvert@gmail.com>
2024-08-20 19:58:20 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+
+// var name = "John";
+// let age = 25;
+// const pi = 3.14;
+
+// console.log(name,age,pi)
+
+// const demo = 1;
+// document.getElementById("demo").innerHTML = "Hello JavaScript!";
